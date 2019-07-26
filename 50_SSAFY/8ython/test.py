8799ffe2cf827797fa5614599b4b2cbfea83e84e
jarvis08 <cdb921226@gmail.com>
2019-07-26 17:40:34 +0900

    week4 day5 update

--- 50_SSAFY/8ython/test.py
+++ 50_SSAFY/8ython/test.py
@@ -1,8 +1,18 @@
-func = lambda x : x ** 2
-print(func(2))
-
-
-a = [1, 2]
-b = [3, 4]
-
-print(list(map(lambda x, y : x + y, a, b)))
+t = int(input())
+for i in range(t):
+    days = int(input())
+    costs = list(map(int, input().split(' ')))
+    max_idx = 0
+    margin = 0    
+    
+    while True:
+        max_idx = costs.index(max(costs))
+        for i in range(max_idx):
+            margin += costs[max_idx] - costs[i]
+        for d in range(max_idx+1):
+            del costs[0]
+        if len(costs) <= 1:
+            break
+        if (days - 1) - (max_idx + 1) <= 0:
+            break
+    print('#{} {}'.format(i+1, margin))
