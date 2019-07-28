6d9d4b59fcb0d99a066ef387f295064637138c5d
jarvis08 <cdb921226@gmail.com>
2019-07-28 23:37:39 +0900

    fixed wrong arrangements of contents in few md files

--- 50_SSAFY/8ython/test.py
+++ 50_SSAFY/8ython/test.py
@@ -1,18 +1,7 @@
-t = int(input())
-for i in range(t):
-    days = int(input())
-    costs = list(map(int, input().split(' ')))
-    max_idx = 0
-    margin = 0    
-    
-    while True:
-        max_idx = costs.index(max(costs))
-        for i in range(max_idx):
-            margin += costs[max_idx] - costs[i]
-        for d in range(max_idx+1):
-            del costs[0]
-        if len(costs) <= 1:
-            break
-        if (days - 1) - (max_idx + 1) <= 0:
-            break
-    print('#{} {}'.format(i+1, margin))
+# global 선언 후 수정
+global_num = 10
+def funct():
+    global global_num
+    global_num = 5
+funct()
+print(global_num)
