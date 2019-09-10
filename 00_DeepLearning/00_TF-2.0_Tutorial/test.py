fe01efd2663ac8dd325fb119612cf35b0e95e08a
jarvis08 <cdb921226@gmail.com>
2019-09-10 22:43:38 +0900

    Docs: add Warning in tf.Variable.md

--- 00_DeepLearning/00_TF-2.0_Tutorial/test.py
+++ 00_DeepLearning/00_TF-2.0_Tutorial/test.py
@@ -3,7 +3,23 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import numpy as np
-
-w = tf.Variable(1)
+import sys
+w = tf.Variable(1.0)
 w.assign_add(1)
+c = tf.constant([[1.0, 2.0], [3.0, 4.0]])
+d = tf.constant([[1.0, 1.0], [0.0, 1.0]])
+e = tf.matmul(c, d)
+# Construct a `Session` to execute the graph.
+sess = tf.compat.v1.Session()
+
+# Execute the graph and store the value that `e` represents in `result`.
+result = sess.run(e)
+print(result)
+
+
+v = tf.Variable(tf.random.truncated_normal([10, 40]))
+# Use `initialized_value` to guarantee that `v` has been
+# initialized before its value is used to initialize `w`.
+# The random values are picked only once.
+w = tf.Variable(v.initialized_value() * 2.0)
 print(w)
