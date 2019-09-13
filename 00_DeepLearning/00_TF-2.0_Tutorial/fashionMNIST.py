bfe64cc6ef89908a4b8f3c7f88751b7a8b554f55
jarvis08 <cdb921226@gmail.com>
2019-09-13 23:28:21 +0900

    Docs: write Callbacks upto 2.5 Reduce LR

--- 00_DeepLearning/00_TF-2.0_Tutorial/fashionMNIST.py
+++ 00_DeepLearning/00_TF-2.0_Tutorial/fashionMNIST.py
@@ -11,10 +11,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
-               'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
-print(train_images.shape)
-print(train_labels)
-print(train_labels.shape)
+ 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
  
 train_images = train_images / 255.0
 test_images = test_images / 255.0
@@ -31,24 +28,36 @@ model.compile(
     metrics=['accuracy']
 )
 
-callbacks = [
-    tf.keras.callbacks.TensorBoard(
-        log_dir='./tensorboard'
-    ),
-    tf.keras.callbacks.ModelCheckpoint(
-        filepath='./checkpoints/fashionMNIST.h5',
-        monitor='accuracy',
-        # save_best_only=True
-    ),
-    tf.keras.callbacks.EarlyStopping(
-        monitor='accuracy',
-        patience=1,
-    )
-    # tf.keras.callbacks.BaseLogger.on_train_begin()
+# # Base Logger
+# train_his = model.fit(train_images, train_labels, epochs=5, callbacks=None)
+# print(train_his.history)
 
-]
+# # Early Stopping
+# callback = tf.keras.callbacks.EarlyStopping(monitor='acc', patience=3)
+# train_his = model.fit(train_images, train_labels, epochs=5, callbacks=[callback])
 
-model.fit(train_images, train_labels, epochs=5, callbacks=callbacks)
+# # Reduce LearningRate on Plateau
+# reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='acc', verbose=1, factor=0.2, patience=5, min_lr=0.001)
+# model.fit(train_images, train_labels, epochs=500, callbacks=[reduce_lr])
+
+
 
 # test_loss, test_acc = model.evaluate(test_images, test_labels)
 # print('test accuracy :', test_acc)
+
+
+
+# callbacks = [
+#     tf.keras.callbacks.TensorBoard(
+#         log_dir='./tensorboard'
+#     ),
+#     tf.keras.callbacks.ModelCheckpoint(
+#         filepath='./checkpoints/fashionMNIST.h5',
+#         monitor='val_loss',
+#         # save_best_only=True
+#     ),
+#     tf.keras.callbacks.EarlyStopping(
+#         monitor='accuracy',
+#         patience=1,
+#     )
+# ]
